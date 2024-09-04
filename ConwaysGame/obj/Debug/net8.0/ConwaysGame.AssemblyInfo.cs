82c116ff3907d172223d5c52e2050414db8592fd
RusticRapter <cmcmullen413@gmail.com>
2024-09-04 14:14:38 -0400

    Set Up

--- ConwaysGame/obj/Debug/net8.0/ConwaysGame.AssemblyInfo.cs
+++ ConwaysGame/obj/Debug/net8.0/ConwaysGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConwaysGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bad4df62d9bca7fe47d947d5961b53155a00fe3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConwaysGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConwaysGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

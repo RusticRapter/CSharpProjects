df5871edf32d0a4ab6adf5043d2d575a52cd864c
RusticRapter <67398862+RusticRapter@users.noreply.github.com>
2024-09-06 10:07:17 -0400

    Changed "neurons" to "weights"
    Changes the array neurons to weights as well as any related terms

--- NeuralNetwork/Code/NeuralNetwork.cs
+++ NeuralNetwork/Code/NeuralNetwork.cs
@@ -3,76 +3,79 @@ using System;
 namespace NeuralNetwork.Code;
 
 public class NeuralNetwork{
-    float[][] neurons { get; }
+    float[][] weights { get; }
+
+    int 
 
     //Generic Constructor
     public NeuralNetwork (int[] layers, string data) {
-        neurons = CreateNeurons(layers);
-        FillNeuronsFromData(data);
+        weights = CreateWeights(layers);
+        FillWeightsFromData(data);
     }
     //Copy Constructor
     //Copies all values from the given network
     public NeuralNetwork (NeuralNetwork network) {
-        int[] layers = new int[network.neurons.Length];
+        int[] layers = new int[network.weights.Length];
         for (int i = 0; i < layers.Length; i++) {
-            layers[i] = network.neurons[i].Length;
+            layers[i] = network.weights[i].Length;
         }
-        neurons = CreateNeurons(layers);
-        network.neurons.CopyTo(neurons, 0);
+        weights = CreateWeights(layers);
+        network.weights.CopyTo(neurons, 0);
     }
 
-    //Creates the layers and neurons
-    float[][] CreateNeurons(int[] layers) {
-        float[][] neurs = new float[layers.Length][];
-        for (int i = 0; i < neurs.Length; i++) {
-            neurs[i] = new float[layers[i]];
+    //Creates the layers and neuron weight
+    float[][] CreateWeights(int[] layers) {
+        float[][] tempWeights = new float[layers.Length][];
+        for (int i = 0; i < tempWeights.Length; i++) {
+            tempWeights[i] = new float[layers[i]];
         }
-        return neurs;
+        return tempWeights;
     }
 
-    //Fills data to each neuron based on the string given
-    //Data in the form of "0.000, 0.000; 0.000, 0.000, 0.000"
-    void FillNeuronsFromData(string data) {
+    //Fills data to each neuron weight based on the string given
+    //Data in the form of "0.000..., 0.000...; 0.000..., 0.000..., 0.000..."
+    void FillWeightsFromData(string data) {
         //Splits each layer apart
         string[] layerStrings = data.Split(';');
 
-        //Splits each neuron apart from the layer
-        string[][] neuronStrings = new string[layerStrings.Length][];
-        for (int i = 0; i < neuronStrings.Length; i++) {
-            neuronStrings[i] = layerStrings[i].Split(',');
+        //Splits each weight apart from the layer
+        string[][] weightStrings = new string[layerStrings.Length][];
+        for (int i = 0; i < weightStrings.Length; i++) {
+            weightStrings[i] = layerStrings[i].Split(',');
         }
 
         //Throws an error if the input data is of the wrong length
-        if (neuronStrings.Length != neurons.Length) {
-            Console.WriteLine("Error in FillNeuronsFromData: Input data length does not match number of neurons");
+        if (weightStrings.Length != weights.Length) {
+            Console.WriteLine("Error in FillWeightsFromData: Input data length does not match number of neurons");
             return;
         }
-        //Sets each neuron
-        for (int i = 0; i < neurons.Length; i++) {
-            for (int j = 0; j < neurons[i].Length; j++) {
-                float neuronValue;
+        //Sets each weight
+        for (int i = 0; i < weights.Length; i++) {
+            for (int j = 0; j < weights[i].Length; j++) {
+                float weightValue;
                 //Throws an error if the input data is not the correct format
                 if (!float.TryParse(neuronStrings[i][j], out neuronValue)) {
-                    Console.WriteLine("Error in FillNeuronsFromData: Input data in invalid format at (" + i + ", " + j + ")");
+                    Console.WriteLine("Error in FillWeightsFromData: Input data in invalid format at (" + i + ", " + j + ")");
                     return;
                 }
-                    neurons[i][j] = neuronValue;
+                    weights[i][j] = weightValue;
             }
         }
     }
 
-    //Changes one neuron to the given value
-    public void ChangeNeuron(int layer, int neuron, float value) {
+    //Changes one neuron weight to the given value
+    public void ChangeNeuronWeight(int layer, int neuron, float value) {
         //Throws an error if the inputted value is not valid
-        if (layer < 0 || neuron < 0 || neurons.Length <= layer || neurons[layer].Length <= neuron) {
-            Console.WriteLine("Error in ChangeNeuron: Input variables invalid");
+        if (layer < 0 || neuron < 0 || weights.Length <= layer || weights[layer].Length <= neuron) {
+            Console.WriteLine("Error in ChangeNeuronWeight: Input variables invalid");
         }
-        neurons[layer][neuron] = value;
+        weights[layer][neuron] = value;
     }
 
     //Computes the input data through the network and outputs a float[] with the values
+    //FINISH
     public float[] Compute(float[] inputs) {
-        float[] output = new float[neurons[neurons.Length-1].Length];
+        float[] output = new float[weights[weights.Length-1].Length];
 
         return output; 
     }
